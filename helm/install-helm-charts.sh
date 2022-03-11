a5f58ebc0611191a3442d71a3999dfae0942e7fb
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2022-03-11 23:59:52 +0100

    clean-up

--- helm/install-helm-charts.sh
+++ helm/install-helm-charts.sh
@@ -11,15 +11,4 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack -f kube-prometheus-stack-values.yaml
-# helm upgrade --install loki-stack grafana/loki-stack --set loki.persistence.enabled=true,loki.persistence.storageClassName=openebs-local-hostpath-data,loki.persistence.size=1Gi
 helm upgrade --install loki-stack grafana/loki-stack -f loki-stack-values.yaml
-
-# helm upgrade --install loki grafana/loki-stack \
-# 	--set loki.persistence.enabled=true,loki.persistence.storageClassName=openebs-local-hostpath-data,loki.persistence.size=1Gi,"promtail.extraScrapeConfigs=- job_name: syslog
-#       syslog:
-#         listen_address: 0.0.0.0:1514
-#         labels:
-#           job: \"syslog\"
-#     relabel_configs:
-#       - source_labels: ['__syslog_message_hostname']
-#         target_label: 'host'",syslogService.enabled=true,syslogService.type=LoadBalancer,syslogService.port=1514
