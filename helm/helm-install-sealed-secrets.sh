6849a91210b16b7d0153f6358fc9e36fe3d87916
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2022-04-03 22:44:30 +0200

    install sealed secrets, first sealed secret

--- helm/helm-install-sealed-secrets.sh
+++ helm/helm-install-sealed-secrets.sh
@@ -9,4 +9,18 @@ cd "$parent_path"
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update
 
-helm install sealed-secrets sealed-secrets/sealed-secrets --set "fullnameOverride=sealed-secrets-controller"
+helm install sealed-secrets sealed-secrets/sealed-secrets --set fullnameOverride=sealed-secrets-controller
+
+# brew install kubeseal
+
+# kubeseal <mysecret.json >mysealedsecret.json --controller-namespace=default
+# OR:
+#kubectl create secret generic secretname --dry-run=client --from-literal=mysecretkey=mysecretvalue -o yaml | \
+#kubeseal \
+#  --controller-name=sealed-secrets-controller \
+#  --controller-namespace=default \
+#  --format yaml > mysealedsecret.yaml
+
+# Backup: kubectl get secret -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml >master.key
+# Restore: kubectl apply -f master.key
+#          kubectl delete pod -l name=sealed-secrets-controller
